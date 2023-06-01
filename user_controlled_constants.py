a3d113424b5c993f5a5910ce53096a7c527a7c47
hydroLB <106267747+hydroLB@users.noreply.github.com>
2023-06-01 13:09:26 -0400

    Update user_controlled_constants.py

--- user_controlled_constants.py
+++ user_controlled_constants.py
@@ -43,7 +43,7 @@ DRIVER1_I2C_ADDRESS = 0x61
 SKIP_SYSTEM_SETUP_WATER_LEVEL = 1.5
 
 # SAVE ANOTHER PLANTS SETTINGS, TO ENSURE THIS IS USED BY THE HYDROPONICS SYSTEM, CHANGE THE NAME OF THE DICTIONARY
-# IN MAIN.PY MAIN FUNCTION to chosen plant name
+# IN MAIN.PY MAIN FUNCTION TO CHOOSEN PLANT VAR NAME (EX: "RASPBERRY_PLANT")
 RASPBERRY_PLANT = {
     'plant_1': {
         'ph_settings': {
@@ -55,8 +55,12 @@ RASPBERRY_PLANT = {
             'dosing_time': [0.1, 0.1, 10]  # [PH_UP_SLEEP_TIME, PH_DOWN_SLEEP_TIME, LOOP_SLEEP_TIME]
         },
         'nutrient_settings': {
-            # INT, INT, INT, INT
-            # Time in seconds for each nute pump to run
+             # Time in seconds for each nute pump to run
+             # 'nutrient_pump_times' specifies the run duration in seconds for each nutrient pump. 
+             # This acts as a 'ratio' selector, influencing the concentration of each nutrient solution.
+             # Values represent the interval each pump is on between each ppm sensor check, not total pump operation time, 
+             # which is dynamically checked using the ppm sensor.
+             # Select a value between 2-5 seconds per pump for optimal dosing. Note that too short a duration may yield only a few drops.
             'nutrient_pump_times': [5, 5, 5, 5],  # MODIFY THE VALUES HERE
             # Margin (in ppm) between the actual target PPM and the first nutrient dosing cycle
             # to avoid overloading the nutrients when the pH is balanced after (which always raises it to some degree).
@@ -80,8 +84,7 @@ RASPBERRY_PLANT = {
     },
 }
 
-
-# SAVE ANOTHER PLANTS SETTINGS, TO ENSURE THIS IS USED BY THE HYDROPONICS SYSTEM, CHANGE THE NAME OF THE DICTIONARY
+# ANOTHER PLANTS SETTINGS (#2), TO ENSURE THIS IS USED BY THE HYDROPONICS SYSTEM, CHANGE THE NAME OF THE DICTIONARY
 # IN MAIN.PY MAIN FUNCTION
 BLUEBERRY_PLANT = {
     'plant': {
@@ -94,8 +97,13 @@ BLUEBERRY_PLANT = {
             'dosing_time': [0.1, 0.1, 10]  # [PH_UP_SLEEP_TIME, PH_DOWN_SLEEP_TIME, LOOP_SLEEP_TIME]
         },
         'nutrient_settings': {
-            # INT, INT, INT, INT
-            # Time in seconds for each nute pump to run
+             # Time in seconds for each nute pump to run
+             # 'nutrient_pump_times' specifies the run duration in seconds for each nutrient pump. 
+             # This acts as a 'ratio' selector, influencing the concentration of each nutrient solution.
+             # Values represent the interval each pump is on between each ppm sensor check, not total pump operation time, 
+             # which is dynamically checked using the ppm sensor.
+             # Select a value between 2-5 seconds per pump for optimal dosing. Note that too short a duration may yield only a few drops.
+                               # INT, INT, INT, INT
             'nutrient_pump_times': [5, 5, 5, 5],  # MODIFY THE VALUES HERE
             # Margin (in ppm) between the actual target PPM and the first nutrient dosing cycle
             # to avoid overloading the nutrients when the pH is balanced after (which always raises it to some degree).
