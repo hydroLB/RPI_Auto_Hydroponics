3dec9ae51c7ff1481edba664fc00bbc1af08b583
hydroLB <leonbouramia@gmail.com>
2024-06-03 22:14:25 -0400

    hopefully handle imports on the raspberry pi

--- main/main.py
+++ main/main.py
@@ -1,6 +1,12 @@
 from time import sleep
+import sys
+import os
 
-from Atlas_and_pump_utilities.AtlasI2C import get_ppm
+# Append the project root directory to PYTHONPATH
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from ..Atlas_and_pump_utilities.AtlasI2C import get_ppm
 from Water_Level_Sensor.WaterSensor import *
 
 from Atlas_and_pump_utilities.pumps import *
